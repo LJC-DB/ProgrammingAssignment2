4539818e482721dc50463f4a5c78a9b5e20d1279
LJC-DB <leonardojcarmo@hotmail.com>
2020-07-13 03:11:06 -0300

    feat: define the inverse calculation

--- cachematrix.R
+++ cachematrix.R
@@ -23,5 +23,13 @@ makeCacheMatrix <- function(x = numeric()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- i$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
